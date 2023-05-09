1ef0f228d797a0e11c693cecef9c049e5f0941b0
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-05-09 12:55:09 +0100

    Rendering data as a components from an array

--- src/App.js
+++ src/App.js
@@ -2,9 +2,39 @@ import './App.css';
 import ContactCard from './components/ContactCard';
 
 function App() {
+
+  const contacts = [
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Peter pan",
+      email: "peter.pan@anythin.com",
+      age: 25
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Paul walker",
+      email: "p.walker@anythin.com",
+      age: 35 
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "xhaka",
+      email: "white.xahka@anythin.com",
+      age: 29 
+    }]
+
   return (
     <div>
-      <ContactCard />
+      {contacts.map((contact, index)=>{
+        return(
+          <ContactCard 
+          avatarUrl={contact.avatarUrl}
+          name={contact.name}
+          email={contact.email}
+          age={contact.age} 
+      />
+        )
+      })}
     </div>
   );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 
 
-const ContactCard = () => {
+const ContactCard = (props) => {
     const [showAge, setShowAge] = useState(false)
 
     return(
         <div className="contact-wrapper">
             <div className="contact-card">
-            <img src="https://via.placeholder.com/150" alt="profile"></img>
+            <img src={props.avatarUrl} alt="profile"></img>
         <div className="user-info">
-            <p>Name: Anything</p>
-            <p>Email: Anything@any.kj</p>
+            <p>Name: {props.name}</p>
+            <p>Email: {props.email}</p>
             <button onClick={()=>setShowAge(!showAge)}>Show Age</button>
-            {showAge? <p>Age: 23</p> : null}
+            {showAge? <p>Age: {props.age}</p> : null}
         </div>
         </div>
     </div>
